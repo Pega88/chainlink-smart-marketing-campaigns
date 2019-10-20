47221895fe6c82f66b655aa1cdb8015a27d51e4f
Niels Buekers <niels.buekers@gmail.com>
2019-10-20 20:01:19 +0200

    json return value

--- app/main.go
+++ app/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"cloud.google.com/go/bigquery"
 	"context"
+	"encoding/json"
 	"fmt"
 	"google.golang.org/api/iterator"
 	"log"
 	"net/http"
-	"cloud.google.com/go/bigquery"
 	"os"
 )
 
 var proj string
+
+type UniqueVisitors struct {
+	UniqueVisitors int64 `json:"uniqueVisitors"`
+}
 
 func main() {
 
@@ -32,51 +37,52 @@ func main() {
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
-//TODO - asdd website param, add date param, add Auth token
+//TODO - add website param, add date param, add Auth token
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-	num := getNumVisitors()
-
-	fmt.Fprint(w,num)
+	w.Header().Set("Content-Type", "application/json")
+	v := UniqueVisitors{getNumUniqueVisitors()}
+	json.NewEncoder(w).Encode(v)
 }
 
 type Visitors struct {
-        Visitors int64 `bigquery:"visitors"`
+	Visitors int64 `bigquery:"visitors"`
 }
 
-func getNumVisitors() int64{
+func getNumUniqueVisitors() int64 {
 	ctx := context.Background()
 	client, err := bigquery.NewClient(ctx, proj)
 	if err != nil {
 		log.Fatal(err)
-        os.Exit(1)
-    }
-    query := client.Query(
-    	`
+		os.Exit(1)
+	}
+	query := client.Query(
+		`
     	SELECT
     	COUNT(DISTINCT fullVisitorId) AS visitors
     	FROM` +
-    	"`chainlink-marketing-roi.ga_export.ga_stream`"+
-    	`WHERE
+			"`chainlink-marketing-roi.ga_export.ga_stream`" +
+			`WHERE
     	trafficsource_source = "chainlink_campaign";`)
-     iter, read_error := query.Read(ctx)
-     if read_error != nil {
-     	log.Fatal(err)
-     }
-     for {
-     	var row Visitors
-        iterError := iter.Next(&row)
-        if iterError == iterator.Done {
-        	return 0
-        }
-        if iterError != nil {
-        	log.Fatal(iterError)
-        	return -1
-        }
-        fmt.Print("unique visitors: %d\n", row.Visitors)
-        return row.Visitors
-    }
+
+	iter, read_error := query.Read(ctx)
+	if read_error != nil {
+		log.Fatal(err)
+	}
+	for {
+		var row Visitors
+		iterError := iter.Next(&row)
+		if iterError == iterator.Done {
+			return 0
+		}
+		if iterError != nil {
+			log.Fatal(iterError)
+			return -1
+		}
+		fmt.Printf("unique visitors: %d", row.Visitors)
+		return row.Visitors
+	}
 }
