858fc4533758aa12b9f3c827710ac0248503da03
Niels Buekers <niels.buekers@gmail.com>
2019-10-25 00:22:07 +0200

    cleanup after SC works

--- truffle-test/node_modules/chainlink/contracts/interfaces/AggregatorInterface.sol
+++
@@ -1,6 +0,0 @@
-pragma solidity 0.4.24;
-
-interface AggregatorInterface {
-  function currentAnswer() external view returns (int256);
-  function updatedHeight() external view returns (uint256);
-}
