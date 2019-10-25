b12777799f47f295207cf6579a470ae2314988df
Niels Buekers <niels.buekers@gmail.com>
2019-10-25 02:34:54 +0200

    deployed job works

--- adapter/deploy.sh
+++ adapter/deploy.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 gcloud functions deploy bq-bridge --runtime go111 --entry-point Handler --trigger-http --project chainlink-marketing-roi
---
+++ adapter/go.mod
@@ -0,0 +1,5 @@
+module bq-bridge
+
+go 1.12
+
+require github.com/linkpoolio/bridges v0.0.0-20191023191308-5ab804c3d3a6
